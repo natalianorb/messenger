78590ff5ecf5433042bc2d8f00894024f3abffd2
Норбоева Наталья Сергеевна <nata.norboeva@gmail.com>
2022-06-12 16:52:52 +0900

    reworked index.js

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const cors = require('cors')
+const schema = require('./schema/schema')
+const dbConnect = require('./database')
+
+const PORT = process.env.PORT || 5000
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}))
+
+async function start () {
+  try {
+    await dbConnect()
+    app.listen(PORT, () => {
+      console.log('Server has been started...')
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
